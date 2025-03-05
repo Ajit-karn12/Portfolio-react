16f747f0ff3a3c3bf6050dd6c78c9d85560b0ac2
Ajit Karn <ajitkarn@MacBookPro.powerhub>
2025-03-05 18:40:27 +0000

    portfolio initial setup

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import {BrowserRouter as Router,Routes, Route} from 'react-router-dom'
+import Header from './Header'
+import Footer from './Footer'
+import './App.css'
+import Home from './Home'
+import Projects from './Projects'
+import About from './About'
+import Skills from './Skills'
+import Contact from './Contact'
+
+export default function App() {
+  return (
+    <Router>
+      <div className='App'>
+      <Header/>
+      <main>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/projects' element={<Projects/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/skills' element={<Skills/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+        </Routes>
+      </main>
+      <Footer/>
+      </div>
+      
+    </Router>
+  )
+}
+
---
+++ src/Contact.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style/contact.css'
+
+function Contact() {
+  return (
+    <div>Contact</div>
+  )
+}
+
+export default Contact
