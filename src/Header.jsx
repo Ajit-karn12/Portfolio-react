ddb0a8820dece3f67f2a133b707604eafde0e22a
Ajit-karn12 <ajitkarn12@gmail.com>
2025-04-25 08:59:38 +0100

    frotend is completed

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { useAnalytics } from './utils/Analytics';
-import './style/header.css';
+import './style/Header.css';
 
 const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false);
